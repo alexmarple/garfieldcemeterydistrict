81ab16d1823b66a5fb4326f252b25ac5d6852018
Alex Marple <54377734+alexmarple@users.noreply.github.com>
2025-08-18 14:36:07 -0400

    Add rules

--- frontend/src/components/Directions/index.jsx
+++ frontend/src/components/Directions/index.jsx
@@ -1,10 +1,10 @@
-import styles from "./Directions.module.css";
-import Map from "../Map";
+import styles from './Directions.module.css';
+import Map from '../Map';
 
 export default function Directions() {
   return (
-    <div id="directions" className={styles.directions}>
-      <div className="box">
+    <div id='directions' className={styles.directions}>
+      <div className='box'>
         <h2>Directions</h2>
 
         <p>
@@ -15,7 +15,7 @@ export default function Directions() {
           pass the Trap and Rifle Range, turn into the cemetery.
         </p>
         <Map />
-        <h2>Find A Gravestone:</h2>
+        <h2>Find A Gravestone</h2>
         <p>Coming soon.</p>
       </div>
     </div>
--- frontend/src/components/Rules/index.jsx
+++ frontend/src/components/Rules/index.jsx
@@ -1,17 +1,19 @@
-import styles from "./Rules.module.css";
+import styles from './Rules.module.css';
 
 const Rules = () => {
   return (
-    <div id="rules" className={styles.rules}>
-      <div className="box">
+    <div id='rules' className={styles.rules}>
+      <div className='box'>
         <h2>Rules</h2>
-        <p>The rules for the cemetery are as follows:</p>
-        <p>Coming soon.</p>
-        {/* <ul className={styles.rulesList}>
-          <li>Rule 1</li>
-          <li>Rule 2</li>
-          <li>Rule 3</li>
-        </ul> */}
+        <ul className={styles.rulesList}>
+          <li>All burials will be accompanied by a burial certificate.</li>
+          <li>
+            All burials will be accomplished by cemetery staff or their
+            designated agents.
+          </li>
+          <li>All burials will be assigned a purchased cemetery plot.</li>
+          <li>All burials will involve a vault, a coffin and or an urn.</li>
+        </ul>
       </div>
     </div>
   );
